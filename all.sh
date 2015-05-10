5285b907967d4bac383183cb789ccbe95a469f33
Yoann Lecuyer <yoann.lecuyer@gmail.com>
2015-05-10 20:07:00 +0200

    Added colors to output

--- all.sh
+++ all.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
+
+echo $(tput setaf 1) #red
 python ./civiweb.py
+echo $(tput setaf 2) #green
 python ./gemalto.py
+echo $(tput setaf 3) #yellow
 python ./goalsystems.py
+echo $(tput setaf 5) #pink
 python ./coordinationsud.py
 
+echo $(tput sgr0) #reset
+
 read -p "Press [Enter] key to exit"
+
+
+
