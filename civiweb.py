bf606366bba9e5cd8144e88311b7f4b7e78ad712
Yoann Lecuyer <yoann.lecuyer@gmail.com>
2015-05-25 14:15:16 +0200

    Print civiweb computing offers in bold

--- civiweb.py
+++ civiweb.py
@@ -5,6 +5,10 @@ import time
 from datetime import datetime
 from pprint import pprint
 
+class color:
+    BOLD = '\033[1m'
+    END = '\033[0m'
+
 CIVIWEB_URL = "https://www.civiweb.com/FR/offre-recherche-avancee/Page/1.aspx?q=s=@c=42,@a=@e=@m=@f=@r=--@v=@t="
 
 r = requests.get(CIVIWEB_URL)
@@ -22,6 +26,22 @@ for result in results:
     compagnie = result.find(class_="location").text[16:]
     poste = result.h1.text[:47]
     url = "https://www.civiweb.com" + result.find(class_='xt_offrelink')['href']
+
+    rb = requests.get(url)
+    soupb = BeautifulSoup(rb.text)
+
+    competences = soupb.find(id="ContenuPrincipal_BlocB1_m_oCompetence").text
+
+    is_informatique = re.search('informatique', competences, re.IGNORECASE)
+
     if len(result.h1.text) > 47:
         poste += "..."
+
+    if is_informatique:
+        print(color.BOLD, end="")
+
     print("\t{0} - {1} - {2} - {3}".format(publie_le, compagnie, poste, url))
+
+    if is_informatique:
+        print(color.END, end="")
+
